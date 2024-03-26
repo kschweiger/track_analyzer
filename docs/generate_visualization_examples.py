12b75040591d534c4505995c596ca08fa9909898
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-03-26 18:05:13 +0100

    doc: Added documentation for summary plots

--- docs/generate_visualization_examples.py
+++ docs/generate_visualization_examples.py
@@ -1,8 +1,20 @@
+import pandas as pd
+
 from geo_track_analyzer.track import GPXFileTrack
 from geo_track_analyzer.utils.base import init_logging
+from geo_track_analyzer.visualize.summary import (
+    plot_segment_box_summary,
+    plot_segment_summary,
+    plot_segment_zones,
+    plot_track_zones,
+)
 
 if __name__ == "__main__":
     init_logging(10)
+
+    import plotly.io as pio
+
+    pio.templates.default = "plotly_dark"
 
     track = GPXFileTrack("tests/resources/Teilstueck_Schau_ins_land.gpx")
 
@@ -56,3 +68,28 @@ if __name__ == "__main__":
         "docs/snippets/examples/visualization/profile_w_segment_borders.html",
         full_html=False,
     )
+
+    data = pd.read_csv("tests/resources/summary_test_data.csv", sep=";")
+
+    for agg in ["time", "distance", "speed"]:
+        plot_track_zones(data, "heartrate", agg, width=None, height=None).write_html(
+            f"docs/snippets/examples/visualization/zone_summary_hr_{agg}.html",
+            full_html=False,
+        )
+
+    plot_segment_zones(data, "heartrate", "time", width=None, height=None).write_html(
+        "docs/snippets/examples/visualization/zone_segment_summary_hr_time.html",
+        full_html=False,
+    )
+
+    for agg in ["total_time", "total_distance", "avg_speed", "max_speed"]:
+        plot_segment_summary(data, agg, width=None, height=None).write_html(
+            f"docs/snippets/examples/visualization/segment_summary_{agg}.html",
+            full_html=False,
+        )
+
+    for metric in ["heartrate", "speed", "elevation"]:
+        plot_segment_box_summary(data, metric, width=None, height=None).write_html(
+            f"docs/snippets/examples/visualization/segment_box_{metric}.html",
+            full_html=False,
+        )
