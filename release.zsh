fb50773977c308b2a0f8fdbf0e5b140a0557752b
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2024-08-01 21:31:33 +0200

    build: Updated release script

--- release.zsh
+++ release.zsh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
 echo "Bumping Version"
-VERSION=$(poetry version --short $1)
+bump-my-version bump $1
 if [[ $? != 0 ]]
 then
-  echo "poetry version failed. Exiting..."
+  echo "Bumping version failed. Exiting..."
   exit 1
 fi
+VERSION=$(bump-my-version show current_version)
+
 git add pyproject.toml
 git commit -n -m "build: Bumped version to ${VERSION} :bookmark:"
 
@@ -22,8 +24,8 @@ git commit -n -m "chore: Updated CHANGELOG.md :memo:"
 
 git tag ${VERSION}
 
-echo "Pushing"
-git push
-
-echo "Pushing tag"
-git push --tag
+# echo "Pushing"
+# git push
+#
+# echo "Pushing tag"
+# git push --tag
