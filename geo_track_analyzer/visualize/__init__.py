14c98b04150c5874fec0eceb52b2cff3d728ee6e
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2025-11-08 09:46:38 +0100

    refactor!: Make plotting code an extra (#30)

--- geo_track_analyzer/visualize/__init__.py
+++ geo_track_analyzer/visualize/__init__.py
@@ -1,18 +1,38 @@
-from .interactive import plot_track_3d
-from .map import (
-    plot_segments_on_map,
-    plot_track_enriched_on_map,
-    plot_track_line_on_map,
-    plot_tracks_on_map,
-)
-from .metrics import plot_metrics
-from .profiles import plot_track_2d, plot_track_with_slope
-from .summary import (
-    plot_segment_box_summary,
-    plot_segment_summary,
-    plot_segment_zones,
-    plot_track_zones,
-)
+try:
+    import plotly  # noqa: F401
+except ModuleNotFoundError:
+
+    def raise_err_func(*args, **kwargs) -> None:
+        raise RuntimeError("Install the visualization extra to use this functionality")
+
+    plot_metrics = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_segment_box_summary = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_segment_summary = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_segment_zones = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_segments_on_map = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_track_2d = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_track_3d = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_track_enriched_on_map = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_track_line_on_map = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_track_with_slope = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_track_zones = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+    plot_tracks_on_map = lambda *args, **kwargs: raise_err_func(*args, **kwargs)
+else:
+    from .interactive import plot_track_3d
+    from .map import (
+        plot_segments_on_map,
+        plot_track_enriched_on_map,
+        plot_track_line_on_map,
+        plot_tracks_on_map,
+    )
+    from .metrics import plot_metrics
+    from .profiles import plot_track_2d, plot_track_with_slope
+    from .summary import (
+        plot_segment_box_summary,
+        plot_segment_summary,
+        plot_segment_zones,
+        plot_track_zones,
+    )
 
 __all__ = [
     "plot_metrics",
